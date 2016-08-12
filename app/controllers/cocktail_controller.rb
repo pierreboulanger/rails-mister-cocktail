b2d494441d6a1bc77e3eef02a5f38d8ef7ce8581
Pierre Boulanger <pierre.boulanger1@gmail.com>
2016-08-12 14:03:20 +0200

    Add figaro - Protect my secret data in config/application.yml

---
+++ app/controllers/cocktail_controller.rb
@@ -0,0 +1,44 @@
+class CocktailController < ApplicationController
+  before_action :set_cocktail, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      flash[:notice] = "New cocktail successfully created !!"
+      redirect_to cocktail_path(@cocktail.id)
+    else
+      flash[:notice] = "Oops something went wrong !!"
+      render "new"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+end
